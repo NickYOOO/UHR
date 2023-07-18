b464dc1f8ff178c744309456d7cd8fc26c3257ca
jinoc <choiae369@gmail.com>
2023-07-18 16:38:22 +0900

    Feat: Button 컴포넌트 추가

--- src/globalStyle/globalStyle.jsx
+++ src/globalStyle/globalStyle.jsx
@@ -3,7 +3,21 @@ import reset from 'styled-reset';
 import '../assets/font/font.css';
 
 const GlobalStyles = createGlobalStyle`
-    ${reset}
-    `;
+  ${reset};
+  h1 {
+    font-family: 'didimmyungjo050';
+  }
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: 'omnigothic030';
+  }
+  p,
+  span {
+    font-family: 'omnigothic020';
+  }
+`;
 
 export default GlobalStyles;
