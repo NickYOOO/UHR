828e767b2eafd7b39dff49acabf148132c594b0d
jiapril11 <tadadadacoding@gmail.com>
2023-07-19 14:07:58 +0900

    Feat: main banner 생성

---
+++ src/components/mainBanner/MainBanner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as Styled from './style';
+
+export default function MainBanner() {
+  return (
+    <Styled.MainBannerSection>
+      <div className="main-text-box">
+        <p>당신의 문화 유산 답사기</p>
+        <p>문화 유산의 매력적인 세계로 여러분을 초대합니다.</p>
+      </div>
+    </Styled.MainBannerSection>
+  );
+}
