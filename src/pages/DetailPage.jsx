7c3000b741fd397561f675418537cb075bb4216a
jeon su jung <sj1910924@icloud.com>
2023-07-20 20:07:07 +0900

    Design: 댓글 리스팅 CSS 레이아웃 초안  작성

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -4,6 +4,7 @@ import HeritageDetail from '../components/heritageDetail/HeritageDetail';
 import HeritageImages from '../components/heritageImages/HeritageImages';
 import CommentForm from '../components/comments/commentForm/CommentForm';
 import KakaoMap from '../components/kakaoMap/KakaoMap';
+import CommentList from '../components/comments/commentList/CommentList';
 
 const MainBoxLayout = styled.div`
   max-width: 1200px;
@@ -18,6 +19,7 @@ function DetailPage() {
       <KakaoMap latitude={37.559975221378} longitude={126.975312652739} />
       <HeritageImages />
       <CommentForm />
+      <CommentList />
     </MainBoxLayout>
   );
 }
