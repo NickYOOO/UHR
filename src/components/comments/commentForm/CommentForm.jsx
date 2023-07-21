d5d3a861d27ff19d8a3d117138183d4b0bb66028
jeon su jung <sj1910924@icloud.com>
2023-07-21 17:03:30 +0900

    Feat: 댓글 수정 및 삭제 기능 추가

--- src/components/comments/commentForm/CommentForm.jsx
+++ src/components/comments/commentForm/CommentForm.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import * as Style from './style';
 import { v4 as uuidv4 } from 'uuid';
 import { auth, getUserInfo } from '../../../api/firebase';
-import currentTime from '../../../utill/currentTime';
+import { HiDotsVertical } from 'react-icons/hi';
+import api from '../../../axios/api';
 
-const CommentForm = ({ hId, hName }) => {
+const CommentForm = ({ hId }) => {
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
   const [userName, setUserName] = useState('');
+  const [selectedComment, setSelectedComment] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [editContent, setEditContent] = useState('');
 
   useEffect(() => {
     fetchComments();
@@ -21,7 +25,7 @@ const CommentForm = ({ hId, hName }) => {
           console.log('오류: ', error);
         });
     }
-  }, [hId]);
+  }, [hId, userName]);
 
   const fetchComments = async () => {
     try {
@@ -42,12 +46,11 @@ const CommentForm = ({ hId, hName }) => {
     const newComment = {
       id: uniqueId,
       hId,
-      hName,
       user: auth.currentUser ? auth.currentUser.uid : null,
       username: userName,
       content: comment,
-      originTime: currentTime(),
-      modifyTime: currentTime(),
+      originTime: new Date().toISOString(),
+      modifyTime: new Date().toISOString(),
     };
 
     try {
@@ -70,6 +73,50 @@ const CommentForm = ({ hId, hName }) => {
     }
   };
 
+  const handleDeleteComment = async commentId => {
+    try {
+      await api.delete(`/comments/${commentId}`);
+      fetchComments();
+    } catch (error) {
+      console.log('댓글 삭제 오류:', error);
+    }
+  };
+
+  const handleEditComment = async () => {
+    const updatedComment = {
+      ...selectedComment,
+      content: editContent,
+      modifyTime: new Date().toISOString(),
+    };
+
+    try {
+      const response = await fetch(`http://localhost:3001/comments/${selectedComment.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedComment),
+      });
+
+      if (response.ok) {
+        setEditMode(false);
+        setEditContent('');
+        setSelectedComment(null);
+        fetchComments();
+      } else {
+        console.log('댓글 수정 실패');
+      }
+    } catch (error) {
+      console.log('댓글 수정 오류:', error);
+    }
+  };
+
+  const hideEditMode = () => {
+    setEditMode(false);
+    setEditContent('');
+    setSelectedComment(null);
+  };
+
   const currentUserUid = auth.currentUser ? auth.currentUser.uid : null;
 
   return (
@@ -88,17 +135,56 @@ const CommentForm = ({ hId, hName }) => {
       <Style.CommentListContainer>
         {comments.map(comment => (
           <Style.CommentItem key={comment.id}>
-            <Style.CommentHeader>
-              <Style.CommentUser>{comment.username}</Style.CommentUser>
-              <Style.CommentTime>{comment.originTime}</Style.CommentTime>
-            </Style.CommentHeader>
-            <Style.CommentContent>{comment.content}</Style.CommentContent>
+            <div>
+              <Style.CommentHeader>
+                <Style.CommentUser>{comment.username}</Style.CommentUser>
+                <Style.CommentTime>{comment.originTime}</Style.CommentTime>
+              </Style.CommentHeader>
+              <Style.CommentContent>{comment.content}</Style.CommentContent>
+            </div>
             {currentUserUid === comment.user && (
-              <Style.MoreOptionsButton>:</Style.MoreOptionsButton>
+              <Style.MoreOptionsButton>
+                <HiDotsVertical
+                  size="25"
+                  color="#000000"
+                  onClick={() => {
+                    setSelectedComment(comment);
+                    setEditMode(false);
+                    setEditContent('');
+                  }}
+                />
+                {selectedComment === comment && (
+                  <Style.OptionsButton>
+                    <button onClick={() => handleDeleteComment(comment.id)}>삭제</button>
+                    <button
+                      onClick={() => {
+                        setEditMode(true);
+                        setEditContent(comment.content);
+                      }}
+                    >
+                      수정
+                    </button>
+                  </Style.OptionsButton>
+                )}
+              </Style.MoreOptionsButton>
             )}
           </Style.CommentItem>
         ))}
       </Style.CommentListContainer>
+      {editMode && (
+        <Style.CommentFormContainer>
+          <Style.CommentLabel htmlFor="editCommentInput">수정 내용 </Style.CommentLabel>
+          <Style.CommentInput
+            id="editCommentInput"
+            type="text"
+            placeholder="댓글을 수정하세요..."
+            value={editContent}
+            onChange={e => setEditContent(e.target.value)}
+          />
+          <Style.CommentButton onClick={handleEditComment}>수정 완료</Style.CommentButton>
+          <Style.CommentButton onClick={hideEditMode}>취소</Style.CommentButton>
+        </Style.CommentFormContainer>
+      )}
     </>
   );
 };
