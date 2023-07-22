19b86af8c60bdf6f9136a6f1255b4cff02d1d16e
jiapril11 <tadadadacoding@gmail.com>
2023-07-22 20:32:14 +0900

    Feat: 목록페이지 페이지네이션 기능 및 리팩토링

---
+++ src/components/common/paginate/Paginate.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+import { useDispatch, useSelector } from 'react-redux';
+import { styled } from 'styled-components';
+import { setCurrentPage } from '../../../redux/modules/extraDataHeritagesBySearchSlice';
+
+export default function Paginate() {
+  const totalCnt = useSelector(state => state.extraDataHeritagesBySearch.totalItems);
+  const items = Array(+totalCnt)
+    .fill()
+    .map((_, i) => i + 1);
+  const itemsPerPage = 10;
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+  const dispatch = useDispatch();
+
+  const handlePageClick = e => {
+    dispatch(setCurrentPage(e.selected + 1));
+  };
+
+  return (
+    <PaginateBox>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel=">"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={10}
+        pageCount={pageCount}
+        previousLabel="<"
+        renderOnZeroPageCount={null}
+        className="react-paginate"
+        activeClassName="active"
+        disabledLinkClassName="inactive"
+      />
+    </PaginateBox>
+  );
+}
+
+const PaginateBox = styled.div`
+  & .react-paginate {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+  }
+
+  & li {
+    cursor: pointer;
+  }
+
+  & li:first-child {
+    margin-right: 20px;
+  }
+
+  & li:last-child {
+    margin-left: 20px;
+  }
+
+  & .active {
+    font-weight: bold;
+    color: #ff7c1d;
+  }
+
+  & .inactive {
+    color: #acacac;
+    cursor: default;
+  }
+`;
--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,134 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { getHeritagesBySearch } from '../api/heritage';
-import { useQuery } from '@tanstack/react-query';
-import Loading from '../components/loading/Loading';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useEffect } from 'react';
+import Paginate from '../components/common/paginate/Paginate';
+import HeritagesTable from '../components/heritagesTable/HeritagesTable';
+import HeritagesListQuery from '../components/heritagesListQuery/HeritagesListQuery';
+import { useSelector } from 'react-redux';
+import Layout from '../components/common/layout/Layout';
+import { styled } from 'styled-components';
 // 검색 컴포넌트 import 해오면 됩니다
 
 const ListPage = () => {
-  const navigate = useNavigate();
-  const params = 'ccbaCtcd=11&ccbaMnm1=탑&pageIndex=1';
-  let totalCnt = 0;
-  let totalPage = 0;
-  let searchedHeritageArr = [];
-
-  const { data, isLoading, isSuccess, isError } = useQuery({
-    queryKey: ['heritageListBySearch'],
-    queryFn: () => getHeritagesBySearch(params),
-  });
+  const currentPage = useSelector(state => state.extraDataHeritagesBySearch.currentPage);
+  const searchedParam = `ccbaCtcd=11&ccbaMnm1=탑&pageIndex=${currentPage}`;
 
   useEffect(() => {
     window.scrollTo(0, 0);
   });
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (isSuccess) {
-    data.map(item => {
-      if (item.name === 'totalCnt') {
-        totalCnt = item.value;
-        totalPage = Math.ceil(totalCnt / 10);
-      }
-    });
-
-    searchedHeritageArr = data.filter(item => item.name === 'item');
-  }
-
-  const handleOnClickTr = (key) => {
-    navigate(`/detail/${key}`);
-  };
+  HeritagesListQuery(searchedParam);
 
   return (
-    <div style={{ padding: '60px 0px' }}>
+    <ListPageBox>
       {/* 검색 컴포넌트 여기에 넣으면 됩니다 */}
 
-      <p>
-        조회된 총 데이터 건 수: <strong style={{ fontWeight: 'bolder' }}>{totalCnt}</strong>
-      </p>
-
-      <TableContainer>
-        <Table>
-          <thead>
-            <TableRow>
-              <TableHeader>순번</TableHeader>
-              <TableHeader>문화재 종목</TableHeader>
-              <TableHeader>문화재명</TableHeader>
-              <TableHeader>지역</TableHeader>
-            </TableRow>
-          </thead>
-          <tbody>
-            {searchedHeritageArr.map(item => {
-              const key = item.children[1].value; // no(key)
-              const num = item.children[0].value; // sn
-              const kind = item.children[2].value.replace(/ >/g, ''); // ccmaName
-              const nameKr = item.children[4].value.replace(/ >/g, ''); // ccbaMnm1
-              const nameCh = item.children[5].value.replace(/ >/g, ''); // ccbaMnm2
-              const city1 = item.children[6].value.replace(/ >/g, ''); // ccbaCtcdNm
-              const city2 = item.children[7].value.replace(/ >/g, ''); // ccsiName
-
-              const ccbaKdcd = item.children[9].value;
-              const ccbaCtcd = item.children[10].value;
-              const ccbaAsno = item.children[11].value;
-              const ccbaCpno = item.children[13].value;
-
-              return (
-                <TableRow
-                  key={key}
-                  onClick={() => handleOnClickTr(ccbaCpno)}
-                >
-                  <TableCell>{num}</TableCell>
-                  <TableCell>{kind}</TableCell>
-                  <TableCell>
-                    {nameKr}({nameCh})
-                  </TableCell>
-                  <TableCell>
-                    {city1} / {city2}
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </tbody>
-        </Table>
-      </TableContainer>
-    </div>
+      <Layout>
+        <HeritagesTable />
+        <Paginate />
+      </Layout>
+    </ListPageBox>
   );
 };
 
 export default ListPage;
 
-const TableContainer = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 20px;
-`;
-
-const Table = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-`;
-
-const TableHeader = styled.th`
-  padding: 10px;
-  border-bottom: 1px solid #000;
-  text-align: left;
-`;
-
-const TableRow = styled.tr`
-  &:nth-child(even) {
-    background-color: #f0f0f0;
-  }
-`;
-
-const TableCell = styled.td`
-  padding: 10px;
-  border-bottom: 1px solid #000;
-  color: #000;
-  font-size: 16px;
-  font-weight: 400;
-  line-height: normal;
-  text-align: left;
+const ListPageBox = styled.div`
+  padding: 60px 0px;
 `;
